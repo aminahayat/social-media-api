ad22585f62b099dfd7ee6403f0b31d8b54328647
Amina Hayat <hayat.amina@hotmail.com>
2022-08-27 19:41:49 +0100

    creating outline

--- models/User.js
+++ models/User.js
@@ -2,3 +2,34 @@ const { Schema, model } = require('mongoose');
 const { isEmail } = require('validator');
 
 const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            validate: [isEmail, "Invalid email format"], // TEST.            
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thoughts",
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Users",
+            }
+        ]
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
