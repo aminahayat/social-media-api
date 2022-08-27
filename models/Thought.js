0251bbd1ee8cc7ecd8995de44b2f19a391ffee30
Amina Hayat <hayat.amina@hotmail.com>
2022-08-27 19:44:35 +0100

    thoights js

---
+++ models/Thought.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const formatDate = require('../utils/formatDate');
+
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAt => formatDate(createdAt),
+        },
+        username: {
+            type: String,
+            required: true,
--- models/User.js
+++ models/User.js
@@ -33,3 +33,15 @@ const userSchema = new Schema(
         },
     }
 );
+// Virtual to track total friend count for the user:
+userSchema.virtual("friendCount").get(() => {
+    if (this.friends) {
+    return this.friends.length;
+    } else if (!this.friends) {
+    return 0;
+    }
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
