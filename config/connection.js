38a70fa86fb8bc59146707d1079c79dd00bbb892
Amina Hayat <hayat.amina@hotmail.com>
2022-08-27 21:48:20 +0100

    code check

--- config/connection.js
+++ config/connection.js
@@ -5,4 +5,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialMed
     useUnifiedTopology: true,
 });
 
-module.exports = mongoose.connection; 
+module.exports = mongoose.connection;
