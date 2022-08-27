219d70367993d611c7da0cbe315f38b9b53ced1d
Amina Hayat <hayat.amina@hotmail.com>
2022-08-27 21:41:42 +0100

    trying to find errors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const db = require("./config/connection.js");
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +11,6 @@ app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
+      console.log(`SOCIAL MEDIA API server is now running on port ${PORT}!`);
     });
-  }); 
-
+  });
