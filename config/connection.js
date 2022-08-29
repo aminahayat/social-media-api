594761cadba065ae5b63cac9d51bdc2e12564a1e
Amina Hayat <hayat.amina@hotmail.com>
2022-08-29 23:15:39 +0100

    fixed error?

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialMediaAPI', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    family: 4
 });
 
 module.exports = mongoose.connection;
